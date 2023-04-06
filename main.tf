95bb41910efbb2fca27aad93d087fa6191445a30
vijaysakhare709 <129182134+vijaysakhare709@users.noreply.github.com>
2023-04-06 12:45:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "api-rg-pro"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "hello" {
+  name                = "api-appserviceplan-pro-vijay"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_linux_web_app" "hello1" {
+  name                = "vijay-tt-yy-s"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  service_plan_id     = azurerm_app_service_plan.hello.id
+
+  site_config {
+  }
+
+depends_on = [
+    azurerm_app_service_plan.hello
+]
+}
+
+
